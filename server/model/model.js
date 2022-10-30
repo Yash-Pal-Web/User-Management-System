8177be423a863e2373b8d45f574ffe9ceb17c538
Yash-Pal-Web <yash9451826087@gmail.com>
2022-10-30 22:48:32 +0530

    second

---
+++ server/model/model.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose');
+
+var schema = new mongoose.Schema({
+    name : {
+        type : String,
+        required: true
+    },
+    email : {
+        type: String,
+        required: true,
+        unique: true
+    },
+    gender : String,
+    status : String
+})
+
+schema.path('email').validate((val) => {
+        emailRegex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+        return emailRegex.test(val);
+    }, 'Invalid e-mail.');
+
+const Userdb = mongoose.model('userdb', schema);
+
+module.exports = Userdb;
+
